fcdcfad677ef8c272ef2bca2ac58f101ba963ada
seo722 <isogood11@naver.com>
2023-11-14 12:31:47 +0900

    5.02

--- src/components/posting/PostingFeed.tsx
+++ src/components/posting/PostingFeed.tsx
@@ -7,7 +7,7 @@ interface PostProps {
 
 const PostingFeed = async ({ posts }: PostProps) => {
   return (
-    <div className="w-full h-full grid grid-cols-1 divide-y justify-around p-4 divide-stone-300 dark:divide-stone-700 pb-10">
+    <div className="w-full h-full grid grid-cols-1 divide-y justify-around p-4 divide-stone-300 dark:divide-stone-700 mb-10">
       {posts.length === 0 && (
         <div className="h-fit flex flex-col justify-center">
           <div className="text-stone-700 dark:text-stone-300">게시글을 남겨주세요!</div>
